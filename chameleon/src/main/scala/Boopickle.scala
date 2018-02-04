443cb8b45a992ca6949241a675e67bdc23c6e7c1
johannes karoff <johannes@karoff.net>
2018-02-05 00:42:29 +0100

    create serializer with deserializer from boopickle pickler

--- chameleon/src/main/scala/Boopickle.scala
+++ chameleon/src/main/scala/Boopickle.scala
@@ -6,10 +6,8 @@ import java.nio.ByteBuffer
 import scala.util.{Failure, Success, Try}
 
 object Boopickle {
-  implicit def boopickleSerializer[T : Pickler]: Serializer[T, ByteBuffer] = new Serializer[T, ByteBuffer] {
+  implicit def boopickleSerializerDeserializer[T : Pickler]: SerializerDeserializer[T, ByteBuffer] = new Serializer[T, ByteBuffer] with Deserializer[T, ByteBuffer] {
     override def serialize(arg: T): ByteBuffer = Pickle.intoBytes(arg)
-  }
-  implicit def boopickleDeserializer[T : Pickler]: Deserializer[T, ByteBuffer] = new Deserializer[T, ByteBuffer] {
     override def deserialize(arg: ByteBuffer): Either[Throwable, T] =
       Try(Unpickle[T].fromBytes(arg)) match {
         case Success(arg) => Right(arg)
