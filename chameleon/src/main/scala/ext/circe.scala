b45b869363da423370bf73a15eb14826ba6031c3
Joan Goyeau <joan@goyeau.com>
2019-05-22 11:48:45 -0700

    String is a poor man's type
    Adding the `Json` type support

--- chameleon/src/main/scala/ext/circe.scala
+++ chameleon/src/main/scala/ext/circe.scala
@@ -10,4 +10,11 @@ object circe {
   implicit def circeDeserializer[T : Decoder]: Deserializer[T, String] = new Deserializer[T, String] {
     override def deserialize(arg: String): Either[Throwable, T] = decode[T](arg)
   }
+
+  implicit def jsonSerializer[T : Encoder]: Serializer[T, Json] = new Serializer[T, Json] {
+    override def serialize(arg: T): Json = arg.asJson
+  }
+  implicit def jsonDeserializer[T : Decoder]: Deserializer[T, Json] = new Deserializer[T, Json] {
+    override def deserialize(arg: Json): Either[Throwable, T] = arg.as[T]
+  }
 }
