8031ab0ccfbaad18cc31861040ac1da9fc18f02b
Scala Steward <me@scala-steward.org>
2019-12-18 19:52:22 +0100

    Update cats-core to 2.1.0

--- project/Deps.scala
+++ project/Deps.scala
@@ -8,7 +8,7 @@ object Deps {
 
   val scalaTest = dep("org.scalatest" %%% "scalatest" % "3.1.0")
   val boopickle = dep("io.suzaku" %%% "boopickle" % "1.3.1")
-  val cats = dep("org.typelevel" %%% "cats-core" % "2.0.0")
+  val cats = dep("org.typelevel" %%% "cats-core" % "2.1.0")
   val circe = new {
     private val version = "0.12.1"
     val core = dep("io.circe" %%% "circe-core" % version)
