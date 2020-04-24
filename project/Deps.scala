24b100b8fecc4d6cc3ad2c33d381fa6dc9686c31
Victor Hugo Borja <vborja@apache.org>
2020-04-23 21:57:07 -0500

    Upgrading to scalajs 1.0.0
    Still waiting for boopickle to be released for scalajs 1.0.0
    See https://github.com/suzaku-io/boopickle/pull/118

--- project/Deps.scala
+++ project/Deps.scala
@@ -10,7 +10,7 @@ object Deps {
   val boopickle = dep("io.suzaku" %%% "boopickle" % "1.3.1")
   val cats = dep("org.typelevel" %%% "cats-core" % "2.1.1")
   val circe = new {
-    private val version = "0.12.1"
+    private val version = "0.13.0"
     val core = dep("io.circe" %%% "circe-core" % version)
     val parser = dep("io.circe" %%% "circe-parser" % version)
   }
@@ -18,5 +18,5 @@ object Deps {
     val core = dep("org.scodec" %%% "scodec-core" % "1.11.7")
     val bits = dep("org.scodec" %%% "scodec-bits" % "1.1.14")
   }
-  val upickle = dep("com.lihaoyi" %%% "upickle" % "0.9.9")
+  val upickle = dep("com.lihaoyi" %%% "upickle" % "1.1.0")
 }
