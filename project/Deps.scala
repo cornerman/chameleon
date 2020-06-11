97b865c3ff11b7d3fb07caed473c55220a7d3057
Victor Hugo Borja <vborja@apache.org>
2020-06-11 08:19:40 -0500

    Use latest boopickle 1.3.3

--- project/Deps.scala
+++ project/Deps.scala
@@ -7,7 +7,7 @@ object Deps {
   import Def.{setting => dep}
 
   val scalaTest = dep("org.scalatest" %%% "scalatest" % "3.1.2")
-  val boopickle = dep("io.suzaku" %%% "boopickle" % "1.3.1")
+  val boopickle = dep("io.suzaku" %%% "boopickle" % "1.3.3")
   val cats = dep("org.typelevel" %%% "cats-core" % "2.1.1")
   val circe = new {
     private val version = "0.13.0"
