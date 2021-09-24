9a1ce05f793094f8af844dc2f51b4936deb8c527
johannes karoff <johannes@karoff.net>
2021-09-25 00:37:30 +0200

    update kind projector

--- chameleon/src/main/scala/ext/cats.scala
+++ chameleon/src/main/scala/ext/cats.scala
@@ -4,13 +4,13 @@ import chameleon._
 import _root_.cats.{Contravariant, Functor, Invariant}
 
 object cats {
-  implicit def contravariantSerializer[PickleType]: Contravariant[Serializer[?, PickleType]] = new Contravariant[Serializer[?, PickleType]] {
+  implicit def contravariantSerializer[PickleType]: Contravariant[Serializer[*, PickleType]] = new Contravariant[Serializer[*, PickleType]] {
     override def contramap[A,B](s: Serializer[A, PickleType])(f: B => A): Serializer[B, PickleType] = s.contramap(f)
   }
-  implicit def functorDeserializer[PickleType]: Functor[Deserializer[?, PickleType]] = new Functor[Deserializer[?, PickleType]] {
+  implicit def functorDeserializer[PickleType]: Functor[Deserializer[*, PickleType]] = new Functor[Deserializer[*, PickleType]] {
     override def map[A,B](s: Deserializer[A, PickleType])(f: A => B): Deserializer[B, PickleType] = s.map(f)
   }
-  implicit def invariantSerializerDeserializer[PickleType]: Invariant[SerializerDeserializer[?, PickleType]] = new Invariant[SerializerDeserializer[?, PickleType]] {
+  implicit def invariantSerializerDeserializer[PickleType]: Invariant[SerializerDeserializer[*, PickleType]] = new Invariant[SerializerDeserializer[*, PickleType]] {
     override def imap[A,B](sd: SerializerDeserializer[A, PickleType])(f: A => B)(g: B => A): SerializerDeserializer[B, PickleType] = sd.imap(f)(g)
   }
 }
