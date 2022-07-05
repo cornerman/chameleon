b06a7631a10a2774be722a51362ed86aa5579278
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-07-05 22:00:29 +0200

    Update scalapb-runtime to 0.11.11 (#179)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- project/Deps.scala
+++ project/Deps.scala
@@ -12,7 +12,7 @@ object Deps {
 
   val cats = dep("org.typelevel" %%% "cats-core" % "2.7.0")
 
-  val scalapb = dep("com.thesamet.scalapb" %%% "scalapb-runtime" % "0.11.10")
+  val scalapb = dep("com.thesamet.scalapb" %%% "scalapb-runtime" % "0.11.11")
 
   val circe = new {
     private val version = "0.14.1"
