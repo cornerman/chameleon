acd45a35f0eb1621ef0deffebc41c1aeb97333ee
Scala Steward <scala_steward@virtuslab.com>
2022-07-13 16:01:40 +0000

    Update jsoniter-scala-core to 2.13.36

--- project/Deps.scala
+++ project/Deps.scala
@@ -25,5 +25,5 @@ object Deps {
     val bits = dep("org.scodec" %%% "scodec-bits" % "1.1.34")
   }
   val upickle = dep("com.lihaoyi" %%% "upickle" % "2.0.0")
-  val jsoniter = dep("com.github.plokhotnyuk.jsoniter-scala" %%% "jsoniter-scala-core" % "2.13.35")
+  val jsoniter = dep("com.github.plokhotnyuk.jsoniter-scala" %%% "jsoniter-scala-core" % "2.13.36")
 }
