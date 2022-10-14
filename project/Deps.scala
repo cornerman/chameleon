8a61df1b438ba658b084768ba1228f5006a3b888
Scala Steward <scala_steward@virtuslab.com>
2022-10-14 17:34:47 +0000

    Update scalatest to 3.2.14

--- project/Deps.scala
+++ project/Deps.scala
@@ -6,7 +6,7 @@ object Deps {
   // hack to expand %%% in settings, needs .value in build.sbt
   import Def.{setting => dep}
 
-  val scalaTest = dep("org.scalatest" %%% "scalatest" % "3.2.13")
+  val scalaTest = dep("org.scalatest" %%% "scalatest" % "3.2.14")
 
   val boopickle = dep("io.suzaku" %%% "boopickle" % "1.4.0")
 
