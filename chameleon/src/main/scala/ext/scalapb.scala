4e1cd516e80c0411dbe4cc5557491e484fdb80ad
johannes karoff <johannes@karoff.net>
2024-06-10 01:38:03 +0200

    provide Array[Byte] Serializer and Deserializer for extensions

--- chameleon/src/main/scala/ext/scalapb.scala
+++ chameleon/src/main/scala/ext/scalapb.scala
@@ -6,7 +6,7 @@ import java.nio.ByteBuffer
 import scala.util.{Failure, Success}
 
 object scalapb {
-  implicit def scalapbSerializerDeserializer[T <: GeneratedMessage](implicit companion: GeneratedMessageCompanion[T]): SerializerDeserializer[T, ByteBuffer] = new Serializer[T, ByteBuffer] with Deserializer[T, ByteBuffer] {
+  implicit def scalapbSerializerDeserializerByteBuffer[T <: GeneratedMessage](implicit companion: GeneratedMessageCompanion[T]): SerializerDeserializer[T, ByteBuffer] = new Serializer[T, ByteBuffer] with Deserializer[T, ByteBuffer] {
     override def serialize(arg: T): ByteBuffer = ByteBuffer.wrap(companion.toByteArray(arg))
     override def deserialize(arg: ByteBuffer): Either[Throwable, T] = {
       val bytes = new Array[Byte](arg.remaining)
@@ -16,5 +16,14 @@ object scalapb {
         case Failure(t) => Left(t)
       }
     }
+  }
+
+  implicit def scalapbSerializerDeserializerByteArray[T <: GeneratedMessage](implicit companion: GeneratedMessageCompanion[T]): SerializerDeserializer[T, Array[Byte]] = new Serializer[T, Array[Byte]] with Deserializer[T, Array[Byte]] {
+    override def serialize(arg: T): Array[Byte] = companion.toByteArray(arg)
+    override def deserialize(bytes: Array[Byte]): Either[Throwable, T] =
+      companion.validate(bytes) match {
+        case Success(arg) => Right(arg)
+        case Failure(t) => Left(t)
+      }
   }
 }
