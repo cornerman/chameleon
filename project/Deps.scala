465bcb399c4ee6631abdd1f34bc0000fff1f23fe
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-12-05 16:09:51 +0100

    Update http4s-core to 0.23.30 (#348)

--- project/Deps.scala
+++ project/Deps.scala
@@ -28,5 +28,5 @@ object Deps {
   val jsoniter = dep("com.github.plokhotnyuk.jsoniter-scala" %%% "jsoniter-scala-core" % "2.31.3")
   val zioJson = dep("dev.zio" %%% "zio-json" % "0.7.3")
 
-  val http4s = dep("org.http4s" %%% "http4s-core" % "0.23.29")
+  val http4s = dep("org.http4s" %%% "http4s-core" % "0.23.30")
 }
