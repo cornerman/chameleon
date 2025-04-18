51021e97b6d0bf634e2e3165a7926a409db69d5f
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-04-18 16:33:36 +0200

    Update jsoniter-scala-core to 2.35.0 (#387)

--- project/Deps.scala
+++ project/Deps.scala
@@ -25,7 +25,7 @@ object Deps {
     val bits = dep("org.scodec" %%% "scodec-bits" % "1.2.1")
   }
   val upickle = dep("com.lihaoyi" %%% "upickle" % "4.1.0")
-  val jsoniter = dep("com.github.plokhotnyuk.jsoniter-scala" %%% "jsoniter-scala-core" % "2.34.1")
+  val jsoniter = dep("com.github.plokhotnyuk.jsoniter-scala" %%% "jsoniter-scala-core" % "2.35.0")
   val zioJson = dep("dev.zio" %%% "zio-json" % "0.7.42")
 
   val http4s = dep("org.http4s" %%% "http4s-core" % "0.23.30")
