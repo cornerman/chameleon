ea433a35310126946b4f57083f281fbe2d1735bd
Scala Steward <scala_steward@virtuslab.com>
2025-05-08 15:55:01 +0000

    Update scodec-core to 1.11.11

--- project/Deps.scala
+++ project/Deps.scala
@@ -20,7 +20,7 @@ object Deps {
     val parser = dep("io.circe" %%% "circe-parser" % version)
   }
   val scodec = new {
-    val core = dep("org.scodec" %%% "scodec-core" % "1.11.10")
+    val core = dep("org.scodec" %%% "scodec-core" % "1.11.11")
     val core2 = dep("org.scodec" %%% "scodec-core" % "2.3.2")
     val bits = dep("org.scodec" %%% "scodec-bits" % "1.2.1")
   }
